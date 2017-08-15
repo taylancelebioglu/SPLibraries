348a531a0c007c4920ca1e70e485410496a77cc9
taylan <taylancelebioglu@gmail.com>
2017-08-15 16:19:28 +0300

    Final

---
+++ SharePoint.Libraries/SharePointTestConsole/TestEntity.cs
@@ -0,0 +1,13 @@
+﻿using SharePoint.Libraries.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointTestConsole
+{
+    public class TestEntity : EntityBase
+    {
+    }
+}
---
+++ SharePoint.Libraries/SharePointTestConsole/TestRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SharePoint;
+using SharePoint.Libraries.Entity;
+
+namespace SharePointTestConsole
+{
+    public class TestRepository : RepositoryBase<TestEntity>
+    {
+        public TestRepository(SPWeb web) : base(web)
+        {
+
+        }
+        protected override string ListUrl => "Lists/test";
+    }
+}
